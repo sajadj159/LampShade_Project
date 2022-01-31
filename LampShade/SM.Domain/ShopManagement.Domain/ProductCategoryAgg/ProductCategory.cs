7cf1e0d0e58c4e5ff14d7869b6db5f842eb5c913
sajad jafari <sajadj159@hotmail.com>
2022-01-31 17:05:41 +0330

    Product Category Domain Model implemented

---
+++ LampShade/SM.Domain/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -0,0 +1,13 @@
+﻿using _0_Framework.Domain;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public class ProductCategory : EntityBase
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string PictureUrl { get; private set; }
+        public string PictureAlt { get; private set; }
+        public string PictureTitle { get; private set; }
+    }
+}
