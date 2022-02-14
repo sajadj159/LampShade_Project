0090f3e7916a2020723b8a56a7dce88b07fac680
sajad jafari <sajadj159@hotmail.com>
2022-02-14 14:10:41 +0330

    script roots now update

--- LampShade/Shopmanagement/SM.Domain/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
+++ LampShade/Shopmanagement/SM.Domain/ShopManagement.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -1,25 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using _0_Framework.Application;
-using _0_Framework.Domain;
+﻿using _0_Framework.Domain;
 
 namespace ShopManagement.Domain.ProductCategoryAgg
 {
     public class ProductCategory : EntityBase
     {
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string PictureUrl { get; private set; }
         public string PictureAlt { get; private set; }
         public string PictureTitle { get; private set; }
-
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Keywords { get; private set; }
-
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string MetaDescription { get; private set; }
-
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Slug { get; private set; }
 
         public ProductCategory(string name, string description, string pictureUrl, string pictureAlt, string pictureTitle, string keywords, string metaDescription, string slug)
