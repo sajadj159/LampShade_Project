0d5e7b8c6d3ccdf4b2581a2f504ba3636967a8c5
sajad jafari <sajadj159@hotmail.com>
2022-02-18 16:40:54 +0330

    ColleagueDiscount Core Implemented

--- LampShade/DiscountManagement/DM.Infrastructure/DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
+++ LampShade/DiscountManagement/DM.Infrastructure/DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
@@ -1,5 +1,8 @@
-﻿using DiscountManagement.Application.A.CustomerDiscount;
+﻿using DiscountManagement.Application.A.ColleagueDiscount;
+using DiscountManagement.Application.A.CustomerDiscount;
+using DiscountManagement.Application.Contract.AC.ColleagueDiscount;
 using DiscountManagement.Application.Contract.AC.CustomerDiscount;
+using DiscountManagement.Domain.ColleagueDiscountAgg;
 using DiscountManagement.Domain.CustomerDiscountAgg;
 using DiscountManagement.Infrastructure.EFCore;
 using DiscountManagement.Infrastructure.EFCore.Repository;
@@ -14,6 +17,9 @@ namespace DiscountManagement.Configuration
         {
             service.AddTransient<ICustomerDiscountApplication, CustomerDiscountApplication>();
             service.AddTransient<ICustomerDiscountRepository, CustomerDiscountRepository>();
+
+            service.AddTransient<IColleagueDiscountApplication, ColleagueDiscountApplication>();
+            service.AddTransient<IColleagueDiscountRepository, ColleagueDiscountRepository>();
 
             service.AddDbContext<DiscountContext>(x => x.UseSqlServer(connectionString));
         }
