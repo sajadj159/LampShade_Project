50a96384cb42ea87309683ed9a2a20afe4783b22
sajad jafari <sajadj159@hotmail.com>
2022-02-19 18:27:22 +0330

    InventoryManagement ui Implemented

--- LampShade/InventoryManagement/IM.Application/InventoryManagement.Application.Contract/AC.Inventory/CreateInventory.cs
+++ LampShade/InventoryManagement/IM.Application/InventoryManagement.Application.Contract/AC.Inventory/CreateInventory.cs
@@ -1,8 +1,12 @@
-﻿namespace InventoryManagement.Application.Contract.AC.Inventory
+﻿using System.Collections.Generic;
+using ShopManagement.Application.Contract.A.Product;
+
+namespace InventoryManagement.Application.Contract.AC.Inventory
 {
     public class CreateInventory
     {
         public long ProductId { get; set; }
         public double UnitPrice { get; set; }
+        public List<ProductViewModel> Products { get; set; }
     }
 }
---
+++ LampShade/ServiceHost/Areas/Administration/Pages/Inventory/Index.cshtml.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using DiscountManagement.Application.Contract.AC.ColleagueDiscount;
+using InventoryManagement.Application.Contract.AC.Inventory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Application.Contract.A.Product;
+
+namespace ServiceHost.Areas.Administration.Pages.Inventory
+{
+    public class IndexModel : PageModel
+    {
+        [TempData] public string Message { get; set; }
+        public List<InventoryViewModel> Inventory;
+        public InventorySearchModel SearchModel;
+        public SelectList Products;
+
+        private readonly IProductApplication _productApplication;
+        private readonly IInventoryApplication _inventoryApplication;
+        public IndexModel(IProductApplication productApplication, IInventoryApplication inventoryApplication)
+        {
+            _productApplication = productApplication;
+            _inventoryApplication = inventoryApplication;
+        }
+
+
+        public void OnGet(InventorySearchModel searchModel)
+        {
+            Products = new SelectList(_productApplication.GetProducts(), "Id", "Name");
+            Inventory = _inventoryApplication.Search(searchModel);
+        }
+
+        public PartialViewResult OnGetCreate()
+        {
+            var command = new CreateInventory
+            {
+                Products = _productApplication.GetProducts()
+            };
+            
+            return Partial("./Create",command);
+        }
+
+        public JsonResult OnPostCreate(CreateInventory command)
+        {
+            var result = _inventoryApplication.Create(command);
+            return new JsonResult(result);
+        }
+
+        public PartialViewResult OnGetEdit(long id)
+        {
+            var editInventory = _inventoryApplication.GetDetails(id);
+           editInventory.Products = _productApplication.GetProducts();
+            return Partial("Edit", editInventory);
+        }
+
+        public JsonResult OnPostEdit(EditInventory command)
+        {
+            var result = _inventoryApplication.Edit(command);
+            return new JsonResult(result);
+        }
+    }
+}
