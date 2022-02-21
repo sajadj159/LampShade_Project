62e6ffea7dbde31afa8c0829afe195872eb8bfab
sajad jafari <sajadj159@hotmail.com>
2022-02-21 18:53:49 +0330

    Menu And Footer Component Implemented

---
+++ LampShade/ServiceHost/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using _01_LampShadeQuery.Contract.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _productCategory;
+
+        public MenuViewComponent(IProductCategoryQuery productCategory)
+        {
+            _productCategory = productCategory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
