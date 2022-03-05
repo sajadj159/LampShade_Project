dca9e02f1f911b4b31a37b31e311ee6a90cc9724
sajad jafari <sajadj159@hotmail.com>
2022-03-05 23:28:57 +0330

    add to cart implemented

--- LampShade/01_LampShadeQuery/Query/ProductQuery.cs
+++ LampShade/01_LampShadeQuery/Query/ProductQuery.cs
@@ -56,27 +56,24 @@ namespace _01_LampShadeQuery.Query
                 }).FirstOrDefault(x => x.Slug == slug);
             if (product == null)
                 return new ProductQueryModel();
-
+            product.Comments = _commentContext.Comments
+                .Where(x => x.Type == CommentType.Product)
+                .Where(x => !x.IsCanceled)
+                .Where(x => x.IsConfirmed)
+                .Where(x => x.OwnerRecordId == product.Id)
+                .Select(x => new CommentQueryModel
+                {
+                    Id = x.Id,
+                    Description = x.Description,
+                    Name = x.Name,
+                    CreationDate = x.CreationDate.ToFarsi()
+                }).OrderByDescending(x => x.Id).ToList();
             var productInventory = inventory.FirstOrDefault(x => x.ProductId == product.Id);
             if (productInventory == null) return product;
             {
                 var price = productInventory.UnitPrice;
                 product.Price = price.ToMoney();
                 product.InStock = productInventory.InStock;
-
-                product.Comments = _commentContext.Comments
-                    .Where(x => x.Type == CommentType.Product)
-                    .Where(x => !x.IsCanceled)
-                    .Where(x => x.IsConfirmed)
-                    .Where(x => x.OwnerRecordId == product.Id)
-                    .Select(x => new CommentQueryModel
-                    {
-                        Id = x.Id,
-                        Description = x.Description,
-                        Name = x.Name,
-                        CreationDate = x.CreationDate.ToFarsi()
-                    }).OrderByDescending(x => x.Id).ToList();
-
                 var productDiscount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
                 if (productDiscount == null) return product;
                 var discountRate = productDiscount.DiscountRate;
--- LampShade/ServiceHost/wwwroot/Theme/assets/js/custom.js
+++ LampShade/ServiceHost/wwwroot/Theme/assets/js/custom.js
@@ -2,14 +2,14 @@
 function addToCart(id, name, price, pictureUrl) {
     debugger;
     let products = $.cookie(cookieName);
-    if (products===undefined) {
+    if (products === undefined) {
         products = [];
     } else {
         products = JSON.parse(products);
     }
     const count = $("#productCount").val();
     const currentProduct = products.find(x => x.id === id);
-    if (currentProduct !==undefined) {
+    if (currentProduct !== undefined) {
         products.find(x => x.id === id).count = parseInt(currentProduct.count) + parseInt(count);
     } else {
         const product = {
@@ -22,4 +22,43 @@ function addToCart(id, name, price, pictureUrl) {
         products.push(product);
     }
     $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
+    updateCart();
+}
+
+function updateCart() {
+    let products = $.cookie(cookieName);
+    products = JSON.parse(products);
+
+    $("#cart_items_count").text(products.length);
+    let cartItemsWrapper = $("#cart_items_wrapper");
+    cartItemsWrapper.html('');
+    products.forEach(x => {
+        const product = `<div class="single-cart-item">
+                                                <a href="javascript:vovalue(0)" class="remove-icon" onClick="removeFromCart('${x.id}')">
+                                                    <i class="ion-android-close"></i>
+                                                </a>
+                                                <div class="image">
+                                                    <a href="single-product.html">
+                                                        <img src="/ProductPictures/${x.pictureUrl}"
+                                                             class="img-fluid" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="content">
+                                                    <p class="product-title">
+                                                        <a href="single-product.html">محصول: ${x.name}</a>
+                                                    </p>
+                                                    <p class="count"><span>تعداد: ${x.count} </span></p>
+                                                    <p class="count"><span>قمیت واحد: ${x.price} </span> تومان</p>
+                                                </div>
+                                            </div>`;
+        cartItemsWrapper.append(product);
+    });
+}
+function removeFromCart(id) {
+    let products = $.cookie(cookieName);
+    products = JSON.parse(products);
+    const itemToRemove = products.findIndex(x => x.id === id);
+    products.splice(itemToRemove, 1);
+    $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
+    updateCart();
 }
