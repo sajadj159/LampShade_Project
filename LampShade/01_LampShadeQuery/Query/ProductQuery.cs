84e6f904fd04dfd914719746c32045bbc6847fe6
sajad jafari <sajadj159@hotmail.com>
2022-03-05 15:12:05 +0330

    cart-items for cookie implemented

--- LampShade/01_LampShadeQuery/Query/ProductQuery.cs
+++ LampShade/01_LampShadeQuery/Query/ProductQuery.cs
@@ -64,6 +64,19 @@ namespace _01_LampShadeQuery.Query
                 product.Price = price.ToMoney();
                 product.InStock = productInventory.InStock;
 
+                product.Comments = _commentContext.Comments
+                    .Where(x => x.Type == CommentType.Product)
+                    .Where(x => !x.IsCanceled)
+                    .Where(x => x.IsConfirmed)
+                    .Where(x => x.OwnerRecordId == product.Id)
+                    .Select(x => new CommentQueryModel
+                    {
+                        Id = x.Id,
+                        Description = x.Description,
+                        Name = x.Name,
+                        CreationDate = x.CreationDate.ToFarsi()
+                    }).OrderByDescending(x => x.Id).ToList();
+
                 var productDiscount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
                 if (productDiscount == null) return product;
                 var discountRate = productDiscount.DiscountRate;
@@ -73,20 +86,6 @@ namespace _01_LampShadeQuery.Query
                 var discountAmount = Math.Round((price * discountRate) / 100);
                 product.PriceWithDiscount = (price - discountAmount).ToMoney();
             }
-
-            product.Comments = _commentContext.Comments
-                .Where(x => x.Type == CommentType.Product)
-                .Where(x => !x.IsCanceled)
-                .Where(x => x.IsConfirmed)
-                .Where(x => x.OwnerRecordId == product.Id)
-                .Select(x => new CommentQueryModel
-                {
-                    Id = x.Id,
-                    Description = x.Description,
-                    Name = x.Name,
-                    CreationDate = x.CreationDate.ToFarsi()
-                }).OrderByDescending(x => x.Id).ToList();
-
             return product;
         }
 
---
+++ LampShade/ServiceHost/wwwroot/Theme/assets/js/custom.js
@@ -0,0 +1,25 @@
+﻿const cookieName = "cart-items";
+function addToCart(id, name, price, pictureUrl) {
+    debugger;
+    let products = $.cookie(cookieName);
+    if (products===undefined) {
+        products = [];
+    } else {
+        products = JSON.parse(products);
+    }
+    const count = $("#productCount").val();
+    const currentProduct = products.find(x => x.id === id);
+    if (currentProduct !==undefined) {
+        products.find(x => x.id === id).count = parseInt(currentProduct.count) + parseInt(count);
+    } else {
+        const product = {
+            id,
+            name,
+            price,
+            pictureUrl,
+            count
+        }
+        products.push(product);
+    }
+    $.cookie(cookieName, JSON.stringify(products), { expires: 2, path: "/" });
+}
